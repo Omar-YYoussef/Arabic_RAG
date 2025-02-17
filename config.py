f64af8503efb32e4dd4d78ba78136f25e3235fcb
Omar Youssef <131721580+Omar-YYoussef@users.noreply.github.com>
2025-02-18 00:40:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 import os
 
 # API Key
-GENAI_API_KEY = 'AIzaSyDhImozXcrhNGslzoGTFgc2P4clpXqVZS0'
+GENAI_API_KEY = 'your_api_key'
 
 # Model Paths
-EMB_MODEL_PATH = r"C:\Users\abdom\.cache\huggingface\hub\models--Omartificial-Intelligence-Space--GATE-AraBert-v1\snapshots\390116f0a0ab00b84cc94c4fd1fa16dfbe293b54"
-RERANKER_MODEL_PATH = r"C:\Users\abdom\.cache\huggingface\hub\models--Omartificial-Intelligence-Space--ARA-Reranker-V1\snapshots\297d34441a505ff8a8f5eddaf352b9cb505dc84c"
+EMB_MODEL_PATH = r"Omartificial-Intelligence-Space--GATE-AraBert-v1"
+RERANKER_MODEL_PATH = r"Omartificial-Intelligence-Space--ARA-Reranker-V1"
 
 # ChromaDB Directory
 CHROMA_PERSIST_DIR = "./chroma_db"
 
 # Poppler Path
-POPPLER_PATH = r"C:\Users\abdom\Downloads\Release-24.08.0-0\poppler-24.08.0\Library\bin"
+POPPLER_PATH = r"Release-24.08.0-0\poppler-24.08.0\Library\bin"
